781f47cf21833da26eda5af5f0235937002b5bed
slayerzeroa <slayerzeroa@naver.com>
2024-05-09 23:31:53 +0900

    feat: attendance web

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # personal-automation
-This repository was created for personal automation work.
+
+1. Summary
+   This repository was created for personal automation work.
+
+2. Environment Setting
+   Each subprojects need env directory(include ID & Password)
+   Each subprojects need ChromeDriver for Selenium
--- blog/main.py
+++ blog/main.py
@@ -10,7 +10,7 @@ contents = get_cnn_news()
 summary = get_summary(contents[1])
 
 translated_title = papago_english_to_korean(contents[0])
-translated_contents = papago_english_to_korean(contents[1])
+translated_contents = papago_english_to_korean(summary)
 url = contents[2]
 
 upload_tistory(translated_title, translated_contents, url)
